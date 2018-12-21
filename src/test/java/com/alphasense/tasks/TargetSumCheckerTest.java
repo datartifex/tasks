86bd22db6ccde7dd4cbeae1da82666a69a68cb4e
datartifex <datartifex@gmail.com>
2018-12-20 23:35:14 -0300

    minor changes

--- src/test/java/com/alphasense/tasks/TargetSumCheckerTest.java
+++ src/test/java/com/alphasense/tasks/TargetSumCheckerTest.java
@@ -28,27 +28,21 @@ public class TargetSumCheckerTest {
 
     @Test
     public void testCheck() throws IOException {
-        // Example 1
         assertTrue(checker.check(INPUT_1.getSource(), OUTPUT_1.getTargetSum()));
         assertFalse(checker.check(INPUT_1.getSource(), OUTPUT_1.getFakeTargetSum()));
         
-        // Example 2
         assertTrue(checker.check(INPUT_2.getSource(), OUTPUT_2.getTargetSum()));
         assertFalse(checker.check(INPUT_2.getSource(), OUTPUT_2.getFakeTargetSum()));
         
-        // Example 3
         assertTrue(checker.check(INPUT_3.getSource(), OUTPUT_3.getTargetSum()));
         assertFalse(checker.check(INPUT_3.getSource(), OUTPUT_3.getFakeTargetSum()));
         
-        // Example 4
         assertTrue(checker.check(INPUT_4.getSource(), OUTPUT_4.getTargetSum()));
         assertFalse(checker.check(INPUT_4.getSource(), OUTPUT_4.getFakeTargetSum()));
         
-        // Example 5
         assertTrue(checker.check(INPUT_5.getSource(), OUTPUT_5.getTargetSum()));
         assertFalse(checker.check(INPUT_5.getSource(), OUTPUT_5.getFakeTargetSum()));
         
-        // Example 6
         assertFalse(checker.check(INPUT_6.getSource(), OUTPUT_6.getTargetSum()));
     }
     
